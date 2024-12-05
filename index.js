4e25d69106445825bbf5eb0d0faafaf3b829381d
RayanHaddadou <rayan.haddadou@ensea.fr>
2024-12-05 14:31:57 +0100

    Commit of Project

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("test from node", Math.random(), new Date())
+const express = require('express')
+const app = express()
+const port = 3000
+const usersRouter = require("./routes/users.js")
+
+
+//middleware
+app.use(express.json())
+//users endpoint
+app.use("/api/", usersRouter)
+
+app.get("/", (req, res) => {
+	res.json({
+		msg: "Welcome to my users API !"
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`)
+})
+
+
+
+
+
+
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+const db = require("../database")
+const {getAllUsers, createNewUser, updateUser, deleteUser} = require("../controllers/usersControllers");
+
+module.exports = router
+// GET METHOD
+router.get("/users", getAllUsers)
+
+
+
+// POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
+router.post("/users",createNewUser)
+
+// PUT : MODIFIER un utilisateur basé sur les données enoyées dans le corps(body) et le paramètre passé dans l'url
+router.put("/:id",updateUser)
+
+//DELETE METHOD
+router.delete("/:id",deleteUser)
